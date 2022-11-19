6a7be0cba50cdf2fb1774387f4b3b6053bddcb34
Paulo Ferreira <paulo.carvalhoferreira@protonmail.com>
2022-11-19 19:07:03 +0000

    api routes setup with rapidAPI

--- build/index.js
+++ build/index.js
@@ -1,3 +1,12 @@
 "use strict";
-console.log("hello");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running at port ${port}`);
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("hello")
+import express, {Express, Request, Response} from "express";
+import dotenv from "dotenv";
+
+
+const app:Express = express();
+
+const port = process.env.PORT || 3000
+
+app.get("/api/jobs", (req: Request, res: Response) => {
+    res.json({msg: "Hello"});
+});
+
+app.post("/api/jobs", (req: Request, res: Response) => {
+    res.json({msg: "Hello post Jobs"});
+});
+
+app.put("/api/jobs/:id", (req: Request, res: Response) => {
+    res.json({msg: "Hello put jobs"});
+});
+
+app.delete("/api/jobs/:id", (req: Request, res: Response) => {
+    res.json({msg: "Hello delete jobs"});
+});
+
+app.listen(port, () =>{
+    console.log(`Server running at port ${port}`);
+});
